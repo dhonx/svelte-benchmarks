39caf750e0243307ba791cf915f9784e63d2f3b6
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-09-09 21:01:47 +0800

    Removes $index that is not needed

--- dbmon/src/App.svelte
+++ dbmon/src/App.svelte
@@ -6,13 +6,13 @@
 
 <table class="table table-striped lastest-data">
   <tbody>
-    {#each databases as db, $index}
+    {#each databases as db}
       <tr>
         <td class="dbname">{db.dbname}</td>
         <td class="query-count">
           <span class={db.lastSample.countClassName}>{db.lastSample.nbQueries}</span>
         </td>
-        {#each db.lastSample.topFiveQueries as q, $index}
+        {#each db.lastSample.topFiveQueries as q}
           <td class="query {q.elapsedClassName}">
             {q.formatElapsed}
             <div class="popover left">
