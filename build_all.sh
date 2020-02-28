176152d5e91a2b9ea9d1d311e042585709484fa9
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-02-29 01:24:39 +0800

    Add builds folder for the build results

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-cd './big-table' && npm run build;
+cd 'big-table' && npm run build;
 cd ..
+cp -r big-table/public builds/big-table
 
-cd './dbmon' && npm run build;
+cd 'dbmon' && npm run build;
 cd ..
+cp -r dbmon/public builds/dbmon
 
-cd './uptime' && npm run build;
+cd 'uptime' && npm run build;
 cd ..
+cp -r uptime/public builds/uptime
 
-cd './svg' && npm run build;
+cd 'svg' && npm run build;
 cd ..
+cp -r svg/public builds/svg
